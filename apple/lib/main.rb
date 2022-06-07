d1162aeadb62a0a9d001b99d2dff99b78b689f85
Griffin Riley <griffin@10-116-43-127.wireless.oregonstate.edu>
2022-06-07 10:42:40 -0700

    applied extract function to apple

--- apple/lib/main.rb
+++ apple/lib/main.rb
@@ -3,12 +3,16 @@
 
 def story
   first = "Mr. Dursley was the director of a firm called Grunnings, which made drills."
+  middle = middletext
+  conclusion = "And that was it."
+  first + middle + conclusion
+end
+
+def middletext
   middle = ""
   middle << "\n"
   middle << "\n"
   middle << "\n"
   middle << "\n"
   middle << "\n"
-  conclusion = "And that was it."
-  first + middle + conclusion
 end
