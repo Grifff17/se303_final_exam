fe97545becaff9ee2c31ab221e5ad8145c1a9845
Griffin Riley <griffin@10-116-43-127.wireless.oregonstate.edu>
2022-06-07 10:55:43 -0700

    applied split loop refactoring to cherry total_and_smallest

--- cherry/lib/main.rb
+++ cherry/lib/main.rb
@@ -5,12 +5,14 @@
 def total_and_smallest
   data = [23, 15, 8, 16, 4, 42]
   smallest = data.first
-  total = 0
   data.each do |d|
-    total += d
     if d < smallest
       smallest = d
     end
+  end
+  total = 0
+  data.each do |d|
+    total += d
   end
   [total, smallest]
 end
